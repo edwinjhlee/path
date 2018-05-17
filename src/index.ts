6b6c40e773d2e6cc9478411e804220c32eb3bd54
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-05-17 13:31:47 +0800

    create write

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import fs from "fs"
+import { resolve } from "url";
 
 export class Path{
     
@@ -15,19 +17,93 @@ export class Path{
         return p.indexOf("/")? p.slice(0, p.length-1) : p
     }
 
-    public exists(): boolean{
-        return true
+    public toFile(): File{
+        return new File(this.path)
     }
 
+    public toDirectory(): Directory{
+        return new Directory(this.path)
+    }
+
+    public exists(): Promise<boolean>{
+        return new Promise((resolve, reject) => {
+            fs.exists(this.path, resolve)
+        })
+    }
+
+    public existsSync(): boolean{
+        return fs.existsSync(this.path)
+    }
 
 }
 
 // No /
 export class File extends Path{
-    // 
+    
+    public async create(){
+        return this.createAndWrite("")
+    }
+
+    public async append(astr: string){
+        if (await this.exists() === false){
+            throw new Error("Not exists")
+        }
+        return new Promise( (resolve, reject) => {
+            fs.appendFile(this.path, astr, (err)=>{
+                if (err) reject(err)
+                else resolve()
+            })
+        })
+    }
+
+    public async write(astr: string){
+        return new Promise( (resolve, reject) => {
+            fs.writeFile(this.path, astr, (err)=>{
+                if (err) reject(err)
+                else resolve()
+            })
+        })
+    }
+
+    public async createAndWrite(astr: string) {
+        if (await this.exists()){
+            throw new Error("Already exists")
+        }
+        return this.write(astr)
+    }
+
+    public readAsString(): string{
+        return ""
+    }
+
+    public getLineReader(){
+        
+    }
+
 }
 
 // With /
 export class Directory extends Path{
     // list files
+
+    public getPathWithSlash(){
+        return this.path + "/"
+    }
+
+    public create(){
+        return new Promise((resolve, reject) => {
+            fs.mkdir(this.path, (e) => {
+                if (e){
+                    reject(e)
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
+
+    public createSync(){
+        return fs.mkdirSync(this.path)
+    }
+
 }
